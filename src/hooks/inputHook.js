11fa75b21e64f0352c8a9ccf466ef891810cb68b
GyunNote <rlaehrbs1968@naver.com>
2024-04-05 17:27:45 +0900

    feat: back-front 연결및 boardList 가져오기

---
+++ src/hooks/inputHook.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function useMaxValueValidateInput(maxSize) {
+    const [inputValue, setInputValue] = useState("");
+
+    const onChange = (e) => {
+        const {value} = e.target;
+
+        if(value.length <= maxSize){
+            setInputValue(() => value);
+        }
+        
+    }
+    return [inputValue, onChange];
+}
--- src/pages/BoardWritePage/BoardWritePage.jsx
+++ src/pages/BoardWritePage/BoardWritePage.jsx
@@ -1,12 +1,45 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import ReactQuill from "react-quill";
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { QUILL_MODULES } from "../../constants/quillModules";
 import { useQuill } from "../../hooks/quillHook";
+import { useMaxValueValidateInput } from "../../hooks/inputHook";
+import { useMutation } from "react-query";
+import { registerStudentBoard } from "../../apis/api/boardApi";
 
 function BoardWritePage(props) {
+    
     const [quillValue , handleQuillValueChange] = useQuill();
+    const [inputValue , handleInputChange] = useMaxValueValidateInput(20);
+    const [num , setNum] = useState(0);
+
+    const registerBoardMutation = useMutation({
+      mutationKey: "registerBoardMutation",
+      mutationFn: registerStudentBoard,
+      onSuccess: response => {
+        alert("글이 작성되었습니다");
+        window.location.replace("/board/student/boardList");
+      }
+    });
+
+    const handleSubmitClick = () => {
+      const board = {
+        studentId: 27,
+        title : inputValue,
+        content : quillValue,
+        theme: "인사",
+        viewCount : 3
+      };
+
+      console.log(board)
+      
+      registerBoardMutation.mutate(board);
+    }
+
+    console.log(inputValue);
+    console.log(quillValue);
+
     return (
         <>
         <div css={s.layout}>
@@ -14,7 +47,7 @@ function BoardWritePage(props) {
             게시글 작성
             
             <div css={s.titleButton}>
-                <button>완료</button>
+                <button onClick={handleSubmitClick}>완료</button>
                 <button>취소</button>
             </div>
           </div>  
@@ -22,7 +55,11 @@ function BoardWritePage(props) {
             <div css={s.imgInsert}>이미지첨부</div>
 
           <div css={s.writeMain}>
-           <input css={s.mainInput} type="text" placeholder="제목을 입력하세요" />
+           <input css={s.mainInput} 
+           type="text" 
+           placeholder="제목을 입력하세요"
+           onChange={handleInputChange}
+           value={inputValue} />
         </div>  
 
             <ReactQuill style={{
